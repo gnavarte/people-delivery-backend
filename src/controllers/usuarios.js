5af6b0fd9cf9ea1676d5435999efb47ee821cf6d
Facundo-Marianelli <facumarianelli@gmail.com>
2023-10-07 20:08:56 -0300

    add email

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -60,7 +60,6 @@ export const updateUsuario = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
-<<<<<<< HEAD
 
 export const addCalificacion = async (req, res) => {
   try {
@@ -94,7 +93,45 @@ export const setStatus = async (req, res) => {
    
     res.status(200).json(user);
   } catch (error) {}
-=======
+};
+export const getUserByEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+    console.log(email)
+    if (!email) {
+      return res.status(400).json({ message: "Debes proporcionar un correo electrónico" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(201).json({ userID: user._id });
+    }
+
+    return res.status(404).json({ message: "Usuario no encontrado" });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error del servidor" });
+  }
+};
+export const  forgotPassword = async (req, res) => {
+  try {
+    const { email,newPassword } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(newPassword, salt);
+    user.password = passwordHash;
+    user.save();
+    return res.status(200).json({ msg: "Password changed successfully. " });
+
+
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+
+}
 export const updatePassword = async (req, res) => {
   try {
     const { email, actualPassword, newPassword } = req.body;
@@ -117,44 +154,3 @@ export const updatePassword = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 }
-export const  forgotPassword = async (req, res) => {
-  try {
-    const { email,newPassword } = req.body;
-    const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(newPassword, salt);
-    user.password = passwordHash;
-    user.save();
-    return res.status(200).json({ msg: "Password changed successfully. " });
-
-
-  }
-  catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-
-}
-
-
-export const getUserByEmail = async (req, res) => {
-  try {
-    const { email } = req.body;
-    console.log(email)
-    if (!email) {
-      return res.status(400).json({ message: "Debes proporcionar un correo electrónico" });
-    }
-    const user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(201).json({ userID: user._id });
-    }
-
-    return res.status(404).json({ message: "Usuario no encontrado" });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error del servidor" });
-  }
->>>>>>> 759d23ee6da8d6a50614c6046b21f7be2ffefb6b
-};
